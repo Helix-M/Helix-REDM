4da6973bdf7c712c5431e26bc4beddf7ef7098e1
LuaFan2 <51294434+LuaFan2@users.noreply.github.com>
2020-08-18 20:48:14 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Helix-REDM
+
+ API that makes RedM-based server development easier 
+ 
+## More about it
+
+-- In Progress...
---
+++ example_fxmanifest.lua
@@ -0,0 +1,10 @@
+ui_page 'ui/index.html'
+
+client_scripts {}
+
+server_scripts {}
+
+files {}
+
+fx_version 'adamant'
+games { 'rdr3' }
